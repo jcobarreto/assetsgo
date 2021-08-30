1edfa0ab0881749c45190ad7ad2e7d33b0b45836
jcobarreto <julio.barreto@gmail.com>
2021-08-30 07:55:15 -0300

    First commit

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,22 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Julio Barreto</Text>
+          <Text color="gray.300" fontSize="small">
+            julio.barreto@gmail.com
+          </Text>
+        </Box>
+      )}
+      
+      <Avatar size="md" name="Julio Barreto" src="https://github.com/jcobarreto.png"/>
+    </Flex>
+  )
+}
